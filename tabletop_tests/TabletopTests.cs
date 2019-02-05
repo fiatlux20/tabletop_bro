09688fe199db9e8e9665247ed61c67f6f137fe67
makeroflight <harlanrhwilliams@gmail.com>
2019-02-04 23:30:46 -0600

    set up some test functions

---
+++ tabletop_tests/TabletopTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tabletop_tests {
+    [TestClass]
+    public class TabletopTests {
+        [TestMethod]
+        public void GetsNumberDice_RNGRoll_ReturnsInt()
+        {
+
+        }
+
+        public void GetsNumberSides_RNGRoll_ReturnsInt()
+        {
+            
+        }
+
+        public void GetsRollModifierOp_RNGRoll_ReturnsTrue()
+        {
+
+        }
+
+        public void GetsRollModifierVal_RNGRoll_ReturnsInt()
+        {
+
+        }
+    }
+}
